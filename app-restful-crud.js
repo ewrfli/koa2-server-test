9c06dd7f823b4afb9341c1f76d319f32331f2688
ewrf3 <q304062@163.com>
2022-01-06 01:15:45 +0800

    add restful crud

---
+++ app-restful-crud.js
@@ -0,0 +1,79 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const bodyparser = require('koa-bodyparser')
+const app = new Koa()
+const router = Router({
+    prefix: '/user'
+})
+
+let userList = [{ username: 'xm', pwd: '123' }]
+
+router.get('/', async(ctx) => {
+    // ctx.body = '<h1>用户首页</h1>'
+    ctx.set("Allow", "GET, POST") //响应头
+    ctx.status = 301 //设置状态码
+    ctx.body = { //发送json
+        code: 201,
+        msg: '请求首页的信息',
+        data: userList
+    }
+})
+
+router.delete('/del', async(ctx) => { //get//localhost:3001/user/del?id=1
+    // let { id } = ctx.require.query
+    // let { id } = ctx.query
+    // console.log('del:', id)
+    // ctx.body = '删除用户'
+
+    let { id } = ctx.request.body
+    userList.splice(Number(id), 1)
+    ctx.body = {
+        code: 200,
+        msg: '删除成功'
+    }
+})
+
+router.post('/add', async(ctx) => { //localhost:3001/user/add json{username:}
+    let { username, pwd } = ctx.request.body
+    userList.push({
+        username,
+        pwd
+    })
+    console.log(userList)
+    ctx.body = {
+        code: 200,
+        msg: '添加成功',
+        data: userList
+    }
+})
+
+
+router.get('/find/:id', async(ctx) => { //localhost:3001/user/find/1
+    let id = ctx.params.id
+    console.log(id)
+    ctx.body = {
+        code: 200,
+        msg: '查询成功',
+        user: userList[Number(id)]
+
+    }
+})
+
+
+router.put('/update', async(ctx) => {
+    let user = ctx.request.body
+    userList.splice(Number(user.id), 1, {
+        username: user.username,
+        pwd: user.pwd
+    })
+    ctx.body = {
+        code: 200,
+        msg: '修改成功',
+        data: userList
+    }
+})
+
+app.use(bodyparser())
+app.use(router.routes())
+
+app.listen(3001)
